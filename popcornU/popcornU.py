ce2e1d3cb66545c444f751cf1b58737b31769939
ZiweiYuan <32089471+ZiweiYuan@users.noreply.github.com>
2018-04-20 20:54:41 -0700

    add keyword search

--- popcornU/popcornU.py
+++ popcornU/popcornU.py
@@ -1,33 +1,53 @@
 from flask import Flask, send_from_directory, render_template, request, url_for
-import config, urllib, json, requests
+import config, urllib, json, requests, re, string
 from firebase import firebase
 
 app = Flask(__name__)
 app.config.from_object(config)
 firebase = firebase.FirebaseApplication('https://popcornu-inf551.firebaseio.com', None)
 
-
 @app.route("/")
-def login():
-    return render_template('login.html')
+def route_login():
+    return render_template('login.html', userInfo = userInfo)
 
-@app.route("/index")
-def index():
+@app.route("/search")
+def route_search():
     results = firebase.get('/Watchlists', None)
     return render_template('popcornU.html', results=results)
 
 @app.route('/results', methods=['POST'])
-def getResults():
-    data = [{}]
+def route_results():
+    nums, keys, results = {}, [], []
     keywords = request.form['keywords']
-    results = firebase.get('/Watchlists', keywords)
-    temp = results
-    # if firebase only return 1 json, it won't be stored as array (it is dict), so we need to convert it
-    try:
-        temp.extend(data)
-    except:
-        results = [temp]
+    if keywords == "":
+        results = firebase.get('/Watchlists', None)
+        return render_template('popcornU.html', results=results)
+    def parseToken(strings):
+        strings = re.sub('[^a-zA-Z0-9]',' ',strings)
+        strings = strings.lower()
+        return strings.split()
+
+    keywordToken = parseToken(keywords)
+    for token in keywordToken:
+        try:
+            if token not in keys:
+                keys.append(token)
+                inums = firebase.get('/Keywords', token)
+                for inum in inums:
+                    if inum in nums.keys():
+                        nums[inum] += 1
+                    else:
+                        nums[inum] = 1
+        except:
+            continue
+
+    nums = sorted(nums.items(),key = lambda x:x[1],reverse = True)
+    for num in nums:
+        result = firebase.get('/Watchlists', num[0])
+        results.append(result)
+
     return render_template('popcornU.html', results=results)
+
 
 if __name__ == "__main__":
     app.run()

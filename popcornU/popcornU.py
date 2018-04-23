2c91336ca24957e584a5d253bed4ca0416780d47
Ding Ding <ding@Dings-MacBook-Pro.local>
2018-04-22 22:41:41 -0700

    some changes

--- popcornU/popcornU.py
+++ popcornU/popcornU.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory, render_template, request, url_for
+from flask import Flask, send_from_directory, render_template, request, url_for, redirect
 import config, urllib, json, requests, re, string
 from firebase import firebase
 from flask_bootstrap import Bootstrap
@@ -14,93 +14,101 @@ years = sorted(firebase.get('/Years', None).keys())
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+	url = 'https://popcornu-user.firebaseio.com/users.json'
+	response = requests.get(url).json()
+	if request.method== 'POST':
+		aa=request.form['key']
+	else:
+		print('nah')
+		aa=request.args.get('key')
+		print(aa)
+	return render_template('login.html', response=response, code=307)
 
 
 @app.route("/search")
 def route_search():
-    results = firebase.get('/Watchlists', None)
-    return render_template('popcornU.html', results=results, types=types, genres=genres, years=years)
+	results = firebase.get('/Watchlists', None)
+	return render_template('popcornU.html', results=results, types=types, genres=genres, years=years)
 
 
 @app.route('/results', methods=['POST'])
 def route_results():
-    nums, keys, results, keywordNum, filterNum = {}, [], [], [], []
-    typeNum, genreNum, yearNum, filters = [], [], [], []
+	nums, keys, results, keywordNum, filterNum = {}, [], [], [], []
+	typeNum, genreNum, yearNum, filters = [], [], [], []
 
-    keywords = request.form['keywords']
-    typesSubmit = request.values.getlist("types")
-    genresSubmit = request.values.getlist("genres")
-    yearsSubmit = request.values.getlist("years")
+	keywords = request.form['keywords']
+	typesSubmit = request.values.getlist("types")
+	genresSubmit = request.values.getlist("genres")
+	yearsSubmit = request.values.getlist("years")
 
-    for x in typesSubmit:
-        filters.append(x)
-        if str(x) != "all types":
-            typeNum.extend(firebase.get('/Types', str(x)))
+	for x in typesSubmit:
+		filters.append(x)
+		if str(x) != "all types":
+			typeNum.extend(firebase.get('/Types', str(x)))
 
-    for genre in genresSubmit:
-        filters.append(genre)
-        if str(genre) != "all genres":
-            genreNum.extend(firebase.get('/Genres', str(genre)))
+	for genre in genresSubmit:
+		filters.append(genre)
+		if str(genre) != "all genres":
+			genreNum.extend(firebase.get('/Genres', str(genre)))
 
-    for year in yearsSubmit:
-        filters.append(year)
-        if str(year) != "all years":
-            yearNum.extend(firebase.get('/Years', str(year)))
+	for year in yearsSubmit:
+		filters.append(year)
+		if str(year) != "all years":
+			yearNum.extend(firebase.get('/Years', str(year)))
 
-    if typeNum != []:
-        filterNum.extend(typeNum)
-    if filterNum != []:
-        if genreNum != []:
-            filterNum = list(set(filterNum).intersection(set(genreNum)))
-    else:
-        filterNum.extend(genreNum)
-    if filterNum != []:
-        if yearNum != []:
-            filterNum = list(set(filterNum).intersection(set(yearNum)))
-    else:
-        filterNum.extend(genreNum)
+	if typeNum != []:
+		filterNum.extend(typeNum)
+	if filterNum != []:
+		if genreNum != []:
+			filterNum = list(set(filterNum).intersection(set(genreNum)))
+	else:
+		filterNum.extend(genreNum)
+	if filterNum != []:
+		if yearNum != []:
+			filterNum = list(set(filterNum).intersection(set(yearNum)))
+	else:
+		filterNum.extend(genreNum)
 
 
-    def parseToken(strings):
-        strings = re.sub('[^a-zA-Z0-9]',' ',strings)
-        strings = strings.lower()
-        return strings.split()
+	def parseToken(strings):
+		strings = re.sub('[^a-zA-Z0-9]',' ',strings)
+		strings = strings.lower()
+		return strings.split()
 
-    if keywords == "":
-        if filterNum == []:
-            results = firebase.get('/Watchlists', None)
-        else:
-            for y in filterNum:
-                result = firebase.get('/Watchlists', y)
-                results.append(result)
-    else:
-        keywordToken = parseToken(keywords)
-        for token in keywordToken:
-            try:
-                if token not in keys:
-                    keys.append(token)
-                    inums = firebase.get('/Keywords', token)
-                    for inum in inums:
-                        if inum in nums.keys():
-                            nums[inum] += 1
-                        else:
-                            nums[inum] = 1
-            except:
-                continue
-        nums = sorted(nums.items(),key = lambda x:x[1],reverse = True)
-        for num in nums:
-            # intersection of facets search and keyword search
-            if filterNum == [] or num[0] in filterNum:
-                result = firebase.get('/Watchlists', num[0])
-                results.append(result)
+	if keywords == "":
+		if filterNum == []:
+			results = firebase.get('/Watchlists', None)
+		else:
+			for y in filterNum:
+				result = firebase.get('/Watchlists', y)
+				results.append(result)
+	else:
+		keywordToken = parseToken(keywords)
+		for token in keywordToken:
+			try:
+				if token not in keys:
+					keys.append(token)
+					inums = firebase.get('/Keywords', token)
+					for inum in inums:
+						if inum in nums.keys():
+							nums[inum] += 1
+						else:
+							nums[inum] = 1
+			except:
+				continue
+		nums = sorted(nums.items(),key = lambda x:x[1],reverse = True)
+		for num in nums:
+			# intersection of facets search and keyword search
+			if filterNum == [] or num[0] in filterNum:
+				result = firebase.get('/Watchlists', num[0])
+				results.append(result)
 
-    return render_template('popcornU.html', results=results, types=types, genres=genres, years=years, keywords=keywords, filters=filters)
+	return render_template('popcornU.html', results=results, types=types, genres=genres, years=years, keywords=keywords, filters=filters)
 
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
